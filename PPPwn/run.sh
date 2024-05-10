ef3c19b707b47b5cf1d79e46c193a38ee3893c7e
DjPopol <djpopol.dev@gmail.com>
2024-05-10 18:27:39 +0200

    Fix Modify pppwn.py is not useful/Add vars STAGE1 & STAGE2
    Modify pppwn.py not needed
    Juste delete "." with:
    '${FIRMWARE_VERSION/.}
    
    Add VARS STAGE1 & STAGE2 (filename without ext)
    So it's easy to configure

--- PPPwn/run.sh
+++ PPPwn/run.sh
@@ -4,7 +4,7 @@
 INTERFACE="eth0" 
 
 # console firmware version [11.00 | 9.00]
-FIRMWAREVERSION="11.00" 
+FIRMWARE_VERSION="11.00" 
 
 # shutdown pi on successful pppwn  [true | false]
 SHUTDOWN=true
@@ -12,6 +12,11 @@ SHUTDOWN=true
 # using a usb to ethernet adapter  [true | false]
 USBETHERNET=false
 
+# name of stage1 BIN"
+STAGE1="stage1_${FIRMWARE_VERSION}"
+
+# name of stage2 BIN"
+STAGE2="stage2_${FIRMWARE_VERSION}"
 
 echo -e "\n\n\033[36m _____  _____  _____                 
 |  __ \\|  __ \\|  __ \\
@@ -31,10 +36,10 @@ if [ $USBETHERNET = true ] ; then
 	coproc read -t 5 && wait "$!" || true
 	sudo ip link set $INTERFACE up
 fi
-echo -e "\n\033[32mReady for console connection\033[92m\nFirmware:\033[93m $FIRMWAREVERSION\033[92m\nInterface:\033[93m $INTERFACE\033[0m\n"
+echo -e "\n\033[32mReady for console connection\033[92m\nFirmware:\033[93m $FIRMWARE_VERSION\033[92m\nInterface:\033[93m $INTERFACE\033[0m\n"
 while [ true ]
 do
-ret=$(sudo python3 /boot/firmware/PPPwn/pppwn.py --interface=$INTERFACE --fw=$FIRMWAREVERSION --stage1=/boot/firmware/PPPwn/stage1_$FIRMWAREVERSION.bin --stage2=/boot/firmware/PPPwn/stage2_$FIRMWAREVERSION.bin)
+ret=$(sudo python3 /boot/firmware/PPPwn/pppwn.py --interface=$INTERFACE --fw=${FIRMWARE_VERSION/.} --stage1=/boot/firmware/PPPwn/$STAGE1.bin --stage2=/boot/firmware/PPPwn/$STAGE2.bin)
 if [ $ret -ge 1 ]
    then
         echo -e "\033[32m\nConsole PPPwned! \033[0m\n"
