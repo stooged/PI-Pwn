f2096bac0bef45c583d63bf884b04913063ca52f
DjPopol <djpopol.dev@gmail.com>
2024-05-11 14:04:28 +0200

    Fix error
     replace ${FIRMWARE_VERSION/.} by ${FIRMWARE_VERSION//.}

--- PPPwn/run.sh
+++ PPPwn/run.sh
@@ -39,7 +39,7 @@ fi
 echo -e "\n\033[32mReady for console connection\033[92m\nFirmware:\033[93m $FIRMWARE_VERSION\033[92m\nInterface:\033[93m $INTERFACE\033[0m\n"
 while [ true ]
 do
-ret=$(sudo python3 /boot/firmware/PPPwn/pppwn.py --interface=$INTERFACE --fw=${FIRMWARE_VERSION/.} --stage1=/boot/firmware/PPPwn/$STAGE1.bin --stage2=/boot/firmware/PPPwn/$STAGE2.bin)
+ret=$(sudo python3 /boot/firmware/PPPwn/pppwn.py --interface=$INTERFACE --fw=${FIRMWARE_VERSION//.} --stage1=/boot/firmware/PPPwn/$STAGE1.bin --stage2=/boot/firmware/PPPwn/$STAGE2.bin)
 if [ $ret -ge 1 ]
    then
         echo -e "\033[32m\nConsole PPPwned! \033[0m\n"
