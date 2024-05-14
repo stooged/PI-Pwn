4f7380639ea8701ece6444a756a18ff7a3615427
George Woodall <georgewoodall82@gmail.com>
2024-05-14 19:11:26 +0100

    Remove option for python version and format using vs-shell-format

--- PPPwn/install.sh
+++ PPPwn/install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 while true; do
-read -p "$(printf '\r\n\r\n\033[36mDo you want the console to connect to the internet after PPPwn? (Y|N):\033[0m ')" pppq
-case $pppq in
-[Yy]* ) 
-sudo apt install pppoe dnsmasq iptables -y
-echo 'bogus-priv
+    read -p "$(printf '\r\n\r\n\033[36mDo you want the console to connect to the internet after PPPwn? (Y|N):\033[0m ')" pppq
+    case $pppq in
+    [Yy]*)
+        sudo apt install pppoe dnsmasq iptables -y
+        echo 'bogus-priv
 expand-hosts
 domain-needed
 server=8.8.8.8
 listen-address=127.0.0.1
 port=5353
 conf-file=/etc/dnsmasq.more.conf' | sudo tee /etc/dnsmasq.conf
-echo 'address=/playstation.com/127.0.0.1
+        echo 'address=/playstation.com/127.0.0.1
 address=/playstation.net/127.0.0.1
 address=/playstation.org/127.0.0.1
 address=/akadns.net/127.0.0.1
@@ -26,8 +26,8 @@ address=/ribob01.net/127.0.0.1
 address=/cddbp.net/127.0.0.1
 address=/nintendo.net/127.0.0.1
 address=/ea.com/127.0.0.1' | sudo tee /etc/dnsmasq.more.conf
-sudo systemctl restart dnsmasq
-echo 'auth
+        sudo systemctl restart dnsmasq
+        echo 'auth
 lcp-echo-failure 3
 lcp-echo-interval 60
 mtu 1482
@@ -38,208 +38,214 @@ netmask 255.255.255.0
 defaultroute
 noipdefault
 usepeerdns' | sudo tee /etc/ppp/pppoe-server-options
-while true; do
-read -p "$(printf '\r\n\r\n\033[36mDo you want to set a PPPoE username and password?\r\nif you select no then these defaults will be used\r\n\r\nUsername: \033[33mppp\r\n\033[36mPassword: \033[33mppp\r\n\r\n\033[36m(Y|N)?: \033[0m')" wapset
-case $wapset in
-[Yy]* ) 
-while true; do
-read -p  "$(printf '\033[33mEnter Username: \033[0m')" PPPU
-case $PPPU in
-"" ) 
- echo -e '\033[31mCannot be empty!\033[0m';;
- * )  
-if grep -q '^[0-9a-zA-Z_ -]*$' <<<$PPPU ; then 
-if [ ${#PPPU} -le 1 ]  || [ ${#PPPU} -ge 33 ] ; then
-echo -e '\033[31mUsername must be between 2 and 32 characters long\033[0m';
-else 
-break;
-fi
-else 
-echo -e '\033[31mUsername must only contain alphanumeric characters\033[0m';
-fi
-esac
+        while true; do
+            read -p "$(printf '\r\n\r\n\033[36mDo you want to set a PPPoE username and password?\r\nif you select no then these defaults will be used\r\n\r\nUsername: \033[33mppp\r\n\033[36mPassword: \033[33mppp\r\n\r\n\033[36m(Y|N)?: \033[0m')" wapset
+            case $wapset in
+            [Yy]*)
+                while true; do
+                    read -p "$(printf '\033[33mEnter Username: \033[0m')" PPPU
+                    case $PPPU in
+                    "")
+                        echo -e '\033[31mCannot be empty!\033[0m'
+                        ;;
+                    *)
+                        if grep -q '^[0-9a-zA-Z_ -]*$' <<<$PPPU; then
+                            if [ ${#PPPU} -le 1 ] || [ ${#PPPU} -ge 33 ]; then
+                                echo -e '\033[31mUsername must be between 2 and 32 characters long\033[0m'
+                            else
+                                break
+                            fi
+                        else
+                            echo -e '\033[31mUsername must only contain alphanumeric characters\033[0m'
+                        fi
+                        ;;
+                    esac
+                done
+                while true; do
+                    read -p "$(printf '\033[33mEnter password: \033[0m')" PPPW
+                    case $PPPW in
+                    "")
+                        echo -e '\033[31mCannot be empty!\033[0m'
+                        ;;
+                    *)
+                        if [ ${#PPPW} -le 1 ] || [ ${#PPPW} -ge 33 ]; then
+                            echo -e '\033[31mPassword must be between 2 and 32 characters long\033[0m'
+                        else
+                            break
+                        fi
+                        ;;
+                    esac
+                done
+                echo -e '\033[36mUsing custom settings\r\n\r\nUsername: \033[33m'$PPPU'\r\n\033[36mPassword: \033[33m'$PPPW'\r\n\r\n\033[0m'
+                break
+                ;;
+            [Nn]*)
+                echo -e '\033[36mUsing default settings\r\n\r\nUsername: \033[33mppp\r\n\033[36mPassword: \033[33mppp\r\n\r\n\033[0m'
+                PPPU="ppp"
+                PPPW="ppp"
+                break
+                ;;
+            *) echo -e '\033[31mPlease answer Y or N\033[0m' ;;
+            esac
+        done
+        echo '"'$PPPU'"  *  "'$PPPW'"  192.168.2.2' | sudo tee /etc/ppp/pap-secrets
+        INET="true"
+        SHTDN="false"
+        echo -e '\033[32mPPPoE installed\033[0m'
+        break
+        ;;
+    [Nn]*)
+        echo -e '\033[35mSkipping PPPoE install\033[0m'
+        INET="false"
+        while true; do
+            read -p "$(printf '\r\n\r\n\033[36mDo you want the pi to shutdown after pwn success\r\n\r\n\033[36m(Y|N)?: \033[0m')" pisht
+            case $pisht in
+            [Yy]*)
+                SHTDN="true"
+                echo -e '\033[32mThe pi will shutdown\033[0m'
+                break
+                ;;
+            [Nn]*)
+                echo -e '\033[35mThe pi will not shutdown\033[0m'
+                SHTDN="false"
+                break
+                ;;
+            *) echo -e '\033[31mPlease answer Y or N\033[0m' ;;
+            esac
+        done
+        break
+        ;;
+    *) echo -e '\033[31mPlease answer Y or N\033[0m' ;;
+    esac
 done
 while true; do
-read -p "$(printf '\033[33mEnter password: \033[0m')" PPPW
-case $PPPW in
-"" ) 
- echo -e '\033[31mCannot be empty!\033[0m';;
- * )  
-if [ ${#PPPW} -le 1 ]  || [ ${#PPPW} -ge 33 ] ; then
-echo -e '\033[31mPassword must be between 2 and 32 characters long\033[0m';
-else 
-break;
-fi
-esac
-done
-echo -e '\033[36mUsing custom settings\r\n\r\nUsername: \033[33m'$PPPU'\r\n\033[36mPassword: \033[33m'$PPPW'\r\n\r\n\033[0m'
-break;;
-[Nn]* ) 
-echo -e '\033[36mUsing default settings\r\n\r\nUsername: \033[33mppp\r\n\033[36mPassword: \033[33mppp\r\n\r\n\033[0m'
- PPPU="ppp"
- PPPW="ppp"
-break;;
-* ) echo -e '\033[31mPlease answer Y or N\033[0m';;
-esac
-done
-echo '"'$PPPU'"  *  "'$PPPW'"  192.168.2.2' | sudo tee /etc/ppp/pap-secrets
-INET="true"
-SHTDN="false"
-echo -e '\033[32mPPPoE installed\033[0m'
-break;;
-[Nn]* ) 
-echo -e '\033[35mSkipping PPPoE install\033[0m'
-INET="false"
-while true; do
-read -p "$(printf '\r\n\r\n\033[36mDo you want the pi to shutdown after pwn success\r\n\r\n\033[36m(Y|N)?: \033[0m')" pisht
-case $pisht in
-[Yy]* ) 
-SHTDN="true"
-echo -e '\033[32mThe pi will shutdown\033[0m'
-break;;
-[Nn]* ) 
-echo -e '\033[35mThe pi will not shutdown\033[0m'
-SHTDN="false"
-break;;
-* ) echo -e '\033[31mPlease answer Y or N\033[0m';;
-esac
-done
-break;;
-* ) echo -e '\033[31mPlease answer Y or N\033[0m';;
-esac
+    read -p "$(printf '\r\n\r\n\033[36mAre you using a usb to ethernet adapter for the console connection\r\n\r\n\033[36m(Y|N)?: \033[0m')" usbeth
+    case $usbeth in
+    [Yy]*)
+        USBE="true"
+        echo -e '\033[32mUsb to ethernet is being used\033[0m'
+        break
+        ;;
+    [Nn]*)
+        echo -e '\033[35mUsb to ethernet is NOT being used\033[0m'
+        USBE="false"
+        break
+        ;;
+    *) echo -e '\033[31mPlease answer Y or N\033[0m' ;;
+    esac
 done
 while true; do
-read -p "$(printf '\r\n\r\n\033[36mAre you using a usb to ethernet adapter for the console connection\r\n\r\n\033[36m(Y|N)?: \033[0m')" usbeth
-case $usbeth in
-[Yy]* ) 
-USBE="true"
-echo -e '\033[32mUsb to ethernet is being used\033[0m'
-break;;
-[Nn]* ) 
-echo -e '\033[35mUsb to ethernet is NOT being used\033[0m'
-USBE="false"
-break;;
-* ) echo -e '\033[31mPlease answer Y or N\033[0m';;
-esac
-done
-while true; do
-read -p "$(printf '\r\n\r\n\033[36mDo you want to use the old python version of pppwn, It is much slower\r\n\r\n\033[36m(Y|N)?: \033[0m')" cppp
-case $cppp in
-[Yy]* ) 
-UCPP="false"
-sudo apt install python3 python3-scapy -y
-echo -e '\033[32mThe Python version of PPPwn is being used\033[0m'
-break;;
-[Nn]* ) 
-echo -e '\033[35mThe C++ version of PPPwn is being used\033[0m'
-UCPP="true"
-break;;
-* ) echo -e '\033[31mPlease answer Y or N\033[0m';;
-esac
-done
-while true; do
-read -p "$(printf '\r\n\r\n\033[36mWould you like to change the firmware version being used, the default is 11.00\r\n\r\n\033[36m(Y|N)?: \033[0m')" fwset
-case $fwset in
-[Yy]* ) 
-while true; do
-read -p  "$(printf '\033[33mEnter the firmware version [11.00 | 9.00]: \033[0m')" FWV
-case $FWV in
-"" ) 
- echo -e '\033[31mCannot be empty!\033[0m';;
- * )  
-if grep -q '^[0-9.]*$' <<<$FWV ; then 
+    read -p "$(printf '\r\n\r\n\033[36mWould you like to change the firmware version being used, the default is 11.00\r\n\r\n\033[36m(Y|N)?: \033[0m')" fwset
+    case $fwset in
+    [Yy]*)
+        while true; do
+            read -p "$(printf '\033[33mEnter the firmware version [11.00 | 9.00]: \033[0m')" FWV
+            case $FWV in
+            "")
+                echo -e '\033[31mCannot be empty!\033[0m'
+                ;;
+            *)
+                if grep -q '^[0-9.]*$' <<<$FWV; then
 
-if [[ ! "$FWV" =~ ^("11.00"|"9.00")$ ]]  ; then
-echo -e '\033[31mThe version must be 11.00 or 9.00\033[0m';
-else 
-break;
-fi
-else 
-echo -e '\033[31mThe version must only contain alphanumeric characters\033[0m';
-fi
-esac
-done
-echo -e '\033[32mYou are using '$FWV'\033[0m'
-break;;
-[Nn]* ) 
-echo -e '\033[35mUsing the default setting: 11.00\033[0m'
-FWV="11.00"
-break;;
-* ) echo -e '\033[31mPlease answer Y or N\033[0m';;
-esac
+                    if [[ ! "$FWV" =~ ^("11.00"|"9.00")$ ]]; then
+                        echo -e '\033[31mThe version must be 11.00 or 9.00\033[0m'
+                    else
+                        break
+                    fi
+                else
+                    echo -e '\033[31mThe version must only contain alphanumeric characters\033[0m'
+                fi
+                ;;
+            esac
+        done
+        echo -e '\033[32mYou are using '$FWV'\033[0m'
+        break
+        ;;
+    [Nn]*)
+        echo -e '\033[35mUsing the default setting: 11.00\033[0m'
+        FWV="11.00"
+        break
+        ;;
+    *) echo -e '\033[31mPlease answer Y or N\033[0m' ;;
+    esac
 done
 ip link
 while true; do
-read -p "$(printf '\r\n\r\n\033[36mWould you like to change the pi lan interface, the default is eth0\r\n\r\n\033[36m(Y|N)?: \033[0m')" ifset
-case $ifset in
-[Yy]* ) 
-while true; do
-read -p  "$(printf '\033[33mEnter the interface value: \033[0m')" IFCE
-case $IFCE in
-"" ) 
- echo -e '\033[31mCannot be empty!\033[0m';;
- * )  
-if grep -q '^[0-9a-zA-Z_ -]*$' <<<$IFCE ; then 
-if [ ${#IFCE} -le 1 ]  || [ ${#IFCE} -ge 17 ] ; then
-echo -e '\033[31mThe interface must be between 2 and 16 characters long\033[0m';
-else 
-break;
-fi
-else 
-echo -e '\033[31mThe interface must only contain alphanumeric characters\033[0m';
-fi
-esac
+    read -p "$(printf '\r\n\r\n\033[36mWould you like to change the pi lan interface, the default is eth0\r\n\r\n\033[36m(Y|N)?: \033[0m')" ifset
+    case $ifset in
+    [Yy]*)
+        while true; do
+            read -p "$(printf '\033[33mEnter the interface value: \033[0m')" IFCE
+            case $IFCE in
+            "")
+                echo -e '\033[31mCannot be empty!\033[0m'
+                ;;
+            *)
+                if grep -q '^[0-9a-zA-Z_ -]*$' <<<$IFCE; then
+                    if [ ${#IFCE} -le 1 ] || [ ${#IFCE} -ge 17 ]; then
+                        echo -e '\033[31mThe interface must be between 2 and 16 characters long\033[0m'
+                    else
+                        break
+                    fi
+                else
+                    echo -e '\033[31mThe interface must only contain alphanumeric characters\033[0m'
+                fi
+                ;;
+            esac
+        done
+        echo -e '\033[32mYou are using '$IFCE'\033[0m'
+        break
+        ;;
+    [Nn]*)
+        echo -e '\033[35mUsing the default setting: eth0\033[0m'
+        IFCE="eth0"
+        break
+        ;;
+    *) echo -e '\033[31mPlease answer Y or N\033[0m' ;;
+    esac
 done
-echo -e '\033[32mYou are using '$IFCE'\033[0m'
-break;;
-[Nn]* ) 
-echo -e '\033[35mUsing the default setting: eth0\033[0m'
-IFCE="eth0"
-break;;
-* ) echo -e '\033[31mPlease answer Y or N\033[0m';;
-esac
-done
-PITYP=$(tr -d '\0' </proc/device-tree/model) 
-if [[ $PITYP == *"Raspberry Pi 4"* ]] || [[ $PITYP == *"Raspberry Pi 5"* ]] ;then
-while true; do
-read -p "$(printf '\r\n\r\n\033[36mDo you want the pi to act as a flash drive to the console\r\n\r\n\033[36m(Y|N)?: \033[0m')" vusb
-case $vusb in
-[Yy]* ) 
-if [ ! -f $HOME/PPPwn/pwndev ]; then
-sudo mkdir $HOME/PPPwn
-sudo dd if=/dev/zero of=$HOME/PPPwn/pwndev bs=4096 count=65535 
-sudo mkdosfs $HOME/PPPwn/pwndev -F 32  
-echo 'dtoverlay=dwc2' | sudo tee -a /boot/firmware/config.txt
-sudo mkdir /media/pwndev
-sudo mount -o loop $HOME/PPPwn/pwndev /media/pwndev
-sudo cp "$HOME/PI-Pwn/USB Drive/goldhen.bin" /media/pwndev
-sudo umount /media/pwndev
-UDEV=$(sudo blkid | grep '^/dev/sd' | cut -f1 -d':')
-if [[ $UDEV == *"dev/sd"* ]] ;then
-sudo mount -o loop $UDEV /media/pwndev
-sudo cp "$HOME/PI-Pwn/USB Drive/goldhen.bin" /media/pwndev
-sudo umount /media/pwndev 
-fi
-fi
-echo -e '\033[32mThe pi will mount as a drive and goldhen.bin has been placed in the drive\n\033[33mYou must plug the pi into the console usb port using the usb-c of the pi\033[0m'
-VUSB="true"
-break;;
-[Nn]* ) 
-echo -e '\033[35mThe pi will not mount as a drive\033[0m'
-VUSB="false"
-break;;
-* ) echo -e '\033[31mPlease answer Y or N\033[0m';;
-esac
-done
+PITYP=$(tr -d '\0' </proc/device-tree/model)
+if [[ $PITYP == *"Raspberry Pi 4"* ]] || [[ $PITYP == *"Raspberry Pi 5"* ]]; then
+    while true; do
+        read -p "$(printf '\r\n\r\n\033[36mDo you want the pi to act as a flash drive to the console\r\n\r\n\033[36m(Y|N)?: \033[0m')" vusb
+        case $vusb in
+        [Yy]*)
+            if [ ! -f $HOME/PPPwn/pwndev ]; then
+                sudo mkdir $HOME/PPPwn
+                sudo dd if=/dev/zero of=$HOME/PPPwn/pwndev bs=4096 count=65535
+                sudo mkdosfs $HOME/PPPwn/pwndev -F 32
+                echo 'dtoverlay=dwc2' | sudo tee -a /boot/firmware/config.txt
+                sudo mkdir /media/pwndev
+                sudo mount -o loop $HOME/PPPwn/pwndev /media/pwndev
+                sudo cp "$HOME/PI-Pwn/USB Drive/goldhen.bin" /media/pwndev
+                sudo umount /media/pwndev
+                UDEV=$(sudo blkid | grep '^/dev/sd' | cut -f1 -d':')
+                if [[ $UDEV == *"dev/sd"* ]]; then
+                    sudo mount -o loop $UDEV /media/pwndev
+                    sudo cp "$HOME/PI-Pwn/USB Drive/goldhen.bin" /media/pwndev
+                    sudo umount /media/pwndev
+                fi
+            fi
+            echo -e '\033[32mThe pi will mount as a drive and goldhen.bin has been placed in the drive\n\033[33mYou must plug the pi into the console usb port using the usb-c of the pi\033[0m'
+            VUSB="true"
+            break
+            ;;
+        [Nn]*)
+            echo -e '\033[35mThe pi will not mount as a drive\033[0m'
+            VUSB="false"
+            break
+            ;;
+        *) echo -e '\033[31mPlease answer Y or N\033[0m' ;;
+        esac
+    done
 else
-VUSB="false"
+    VUSB="false"
 fi
 echo '#!/bin/bash
 INTERFACE="'$IFCE'" 
 FIRMWAREVERSION="'$FWV'" 
 SHUTDOWN='$SHTDN'
 USBETHERNET='$USBE'
-USECPP='$UCPP'
 PPPOECONN='$INET'
 VMUSB='$VUSB'' | sudo tee /boot/firmware/PPPwn/config.sh
 sudo rm /usr/lib/systemd/system/bluetooth.target
@@ -255,14 +261,14 @@ Environment=NODE_ENV=production
 [Install]
 WantedBy=multi-user.target' | sudo tee /etc/systemd/system/pipwn.service
 if [ -f /boot/firmware/PPPwn/pwndev ]; then
-sudo rm /boot/firmware/PPPwn/pwndev
-sudo mkdir $HOME/PPPwn
-sudo dd if=/dev/zero of=$HOME/PPPwn/pwndev bs=4096 count=65535 
-sudo mkdosfs $HOME/PPPwn/pwndev -F 32 
-sudo mkdir /media/pwndev
-sudo mount -o loop $HOME/PPPwn/pwndev /media/pwndev
-sudo cp "$HOME/PI-Pwn/USB Drive/goldhen.bin" /media/pwndev
-sudo umount /media/pwndev
+    sudo rm /boot/firmware/PPPwn/pwndev
+    sudo mkdir $HOME/PPPwn
+    sudo dd if=/dev/zero of=$HOME/PPPwn/pwndev bs=4096 count=65535
+    sudo mkdosfs $HOME/PPPwn/pwndev -F 32
+    sudo mkdir /media/pwndev
+    sudo mount -o loop $HOME/PPPwn/pwndev /media/pwndev
+    sudo cp "$HOME/PI-Pwn/USB Drive/goldhen.bin" /media/pwndev
+    sudo umount /media/pwndev
 fi
 sudo chmod u+rwx /etc/systemd/system/pipwn.service
 sudo systemctl enable pipwn
--- PPPwn/run.sh
+++ PPPwn/run.sh
@@ -1,54 +1,50 @@
 #!/bin/bash
 
 if [ ! -f /boot/firmware/PPPwn/config.sh ]; then
-INTERFACE="eth0" 
-FIRMWAREVERSION="11.00" 
-SHUTDOWN=true
-USBETHERNET=false
-PPPOECONN=false
-USECPP=true
-VMUSB=false
+	INTERFACE="eth0"
+	FIRMWAREVERSION="11.00"
+	SHUTDOWN=true
+	USBETHERNET=false
+	PPPOECONN=false
+	VMUSB=false
 else
-source /boot/firmware/PPPwn/config.sh
+	source /boot/firmware/PPPwn/config.sh
 fi
-if [[ -z $USECPP ]] ;then
-USECPP=true
-fi
-PITYP=$(tr -d '\0' </proc/device-tree/model) 
-if [[ $PITYP == *"Raspberry Pi 2"* ]] ;then
-coproc read -t 15 && wait "$!" || true
-CPPBIN="pppwn7"
-VMUSB=false
-elif [[ $PITYP == *"Raspberry Pi 3"* ]] ;then
-coproc read -t 10 && wait "$!" || true
-CPPBIN="pppwn64"
-VMUSB=false
-elif [[ $PITYP == *"Raspberry Pi 4"* ]] ;then
-coproc read -t 5 && wait "$!" || true
-CPPBIN="pppwn64"
-elif [[ $PITYP == *"Raspberry Pi 5"* ]] ;then
-coproc read -t 5 && wait "$!" || true
-CPPBIN="pppwn64"
-elif [[ $PITYP == *"Raspberry Pi Zero 2"* ]] ;then
-coproc read -t 8 && wait "$!" || true
-CPPBIN="pppwn64"
-VMUSB=false
-elif [[ $PITYP == *"Raspberry Pi Zero"* ]] ;then
-coproc read -t 10 && wait "$!" || true
-CPPBIN="pppwn11"
-VMUSB=false
-elif [[ $PITYP == *"Raspberry Pi"* ]] ;then
-coproc read -t 15 && wait "$!" || true
-CPPBIN="pppwn11"
-VMUSB=false
+PITYP=$(tr -d '\0' </proc/device-tree/model)
+if [[ $PITYP == *"Raspberry Pi 2"* ]]; then
+	coproc read -t 15 && wait "$!" || true
+	CPPBIN="pppwn7"
+	VMUSB=false
+elif [[ $PITYP == *"Raspberry Pi 3"* ]]; then
+	coproc read -t 10 && wait "$!" || true
+	CPPBIN="pppwn64"
+	VMUSB=false
+elif [[ $PITYP == *"Raspberry Pi 4"* ]]; then
+	coproc read -t 5 && wait "$!" || true
+	CPPBIN="pppwn64"
+elif [[ $PITYP == *"Raspberry Pi 5"* ]]; then
+	coproc read -t 5 && wait "$!" || true
+	CPPBIN="pppwn64"
+elif [[ $PITYP == *"Raspberry Pi Zero 2"* ]]; then
+	coproc read -t 8 && wait "$!" || true
+	CPPBIN="pppwn64"
+	VMUSB=false
+elif [[ $PITYP == *"Raspberry Pi Zero"* ]]; then
+	coproc read -t 10 && wait "$!" || true
+	CPPBIN="pppwn11"
+	VMUSB=false
+elif [[ $PITYP == *"Raspberry Pi"* ]]; then
+	coproc read -t 15 && wait "$!" || true
+	CPPBIN="pppwn11"
+	VMUSB=false
 else
-coproc read -t 5 && wait "$!" || true
-CPPBIN="pppwn64"
-VMUSB=false
+	coproc read -t 5 && wait "$!" || true
+	CPPBIN="pppwn64"
+	VMUSB=false
 fi
 arch=$(getconf LONG_BIT)
-if [ $arch -eq 32 ] && [ $CPPBIN = "pppwn64" ] ; then
-CPPBIN="pppwn7"
+if [ $arch -eq 32 ] && [ $CPPBIN = "pppwn64" ]; then
+	CPPBIN="pppwn7"
 fi
 echo -e "\n\n\033[36m _____  _____  _____                 
 |  __ \\|  __ \\|  __ \\
@@ -57,71 +53,60 @@ echo -e "\n\n\033[36m _____  _____  _____
 | |    | |    | |     \\ V  V /| | | |
 |_|    |_|    |_|      \\_/\\_/ |_| |_|\033[0m
 \n\033[33mhttps://github.com/TheOfficialFloW/PPPwn\033[0m\n" | sudo tee /dev/tty1
-if [ $USBETHERNET = true ] ; then
+if [ $USBETHERNET = true ]; then
 	echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind
 	coproc read -t 2 && wait "$!" || true
 	echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind
 	coproc read -t 5 && wait "$!" || true
 	sudo ip link set $INTERFACE up
-   else	
+else
 	sudo ip link set $INTERFACE down
 	coproc read -t 5 && wait "$!" || true
 	sudo ip link set $INTERFACE up
 fi
 echo -e "\n\033[36m$PITYP\033[92m\nFirmware:\033[93m $FIRMWAREVERSION\033[92m\nInterface:\033[93m $INTERFACE\033[0m" | sudo tee /dev/tty1
-if [ $USECPP = true ] ; then
-   echo -e "\033[92mPPPwn:\033[93m C++ $CPPBIN \033[0m" | sudo tee /dev/tty1
-else
-   echo -e "\033[92mPPPwn:\033[93m Python pppwn.py \033[0m" | sudo tee /dev/tty1
-fi
-if [ $VMUSB = true ] ; then
-   UDEV=$(sudo blkid | grep '^/dev/sd' | cut -f1 -d':')
-   if [[ -z $UDEV ]] ;then
-      UDEV="$HOME/PPPwn/pwndev"
-	  echo -e "\033[92mVirtual Drive:\033[93m Enabled\033[0m" | sudo tee /dev/tty1
+echo -e "\033[92mPPPwn:\033[93m C++ $CPPBIN \033[0m" | sudo tee /dev/tty1
+if [ $VMUSB = true ]; then
+	UDEV=$(sudo blkid | grep '^/dev/sd' | cut -f1 -d':')
+	if [[ -z $UDEV ]]; then
+		UDEV="$HOME/PPPwn/pwndev"
+		echo -e "\033[92mVirtual Drive:\033[93m Enabled\033[0m" | sudo tee /dev/tty1
 	else
-	  echo -e "\033[92mFlash Drive:\033[93m Enabled\033[0m" | sudo tee /dev/tty1
-   fi
-   sudo modprobe g_mass_storage file=$UDEV stall=0 ro=0 removable=1
+		echo -e "\033[92mFlash Drive:\033[93m Enabled\033[0m" | sudo tee /dev/tty1
+	fi
+	sudo modprobe g_mass_storage file=$UDEV stall=0 ro=0 removable=1
 fi
-if [ $PPPOECONN = true ] ; then
-   echo -e "\033[92mInternet Access:\033[93m Enabled\033[0m" | sudo tee /dev/tty1
-else   
-   echo -e "\033[92mInternet Access:\033[93m Disabled\033[0m" | sudo tee /dev/tty1
+if [ $PPPOECONN = true ]; then
+	echo -e "\033[92mInternet Access:\033[93m Enabled\033[0m" | sudo tee /dev/tty1
+else
+	echo -e "\033[92mInternet Access:\033[93m Disabled\033[0m" | sudo tee /dev/tty1
 fi
 if [[ ! $(ethtool $INTERFACE) == *"Link detected: yes"* ]]; then
-   echo -e "\033[31mWaiting for link\033[0m" | sudo tee /dev/tty1
-   while [[ ! $(ethtool $INTERFACE) == *"Link detected: yes"* ]]
-   do
-      coproc read -t 2 && wait "$!" || true
-   done
-   echo -e "\033[32mLink found\033[0m\n" | sudo tee /dev/tty1
+	echo -e "\033[31mWaiting for link\033[0m" | sudo tee /dev/tty1
+	while [[ ! $(ethtool $INTERFACE) == *"Link detected: yes"* ]]; do
+		coproc read -t 2 && wait "$!" || true
+	done
+	echo -e "\033[32mLink found\033[0m\n" | sudo tee /dev/tty1
 fi
 PIIP=$(hostname -I) || true
 if [ "$PIIP" ]; then
-   echo -e "\n\033[92mIP: \033[93m $PIIP\033[0m" | sudo tee /dev/tty1
+	echo -e "\n\033[92mIP: \033[93m $PIIP\033[0m" | sudo tee /dev/tty1
 fi
 echo -e "\n\033[95mReady for console connection\033[0m\n" | sudo tee /dev/tty1
-while [ true ]
-do
-if [ $USECPP = true ] ; then
-   ret=$(sudo /boot/firmware/PPPwn/$CPPBIN --interface "$INTERFACE" --fw "${FIRMWAREVERSION//.}" --stage1 "/boot/firmware/PPPwn/stage1_$FIRMWAREVERSION.bin" --stage2 "/boot/firmware/PPPwn/stage2_$FIRMWAREVERSION.bin")
-else
-   ret=$(sudo python3 /boot/firmware/PPPwn/pppwn.py --interface=$INTERFACE --fw=$FIRMWAREVERSION --stage1=/boot/firmware/PPPwn/stage1_$FIRMWAREVERSION.bin --stage2=/boot/firmware/PPPwn/stage2_$FIRMWAREVERSION.bin)
-fi
-if [ $ret -ge 1 ]
-   then
-        echo -e "\033[32m\nConsole PPPwned! \033[0m\n" | sudo tee /dev/tty1
-		if [ $PPPOECONN = true ] ; then
-		    if [ $USBETHERNET = true ] ; then
-		     echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind
-        	 coproc read -t 3 && wait "$!" || true
-        	 echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind
-		    else	
-        	 sudo ip link set $INTERFACE down
-        	 coproc read -t 3 && wait "$!" || true
-        	 sudo ip link set $INTERFACE up
-		    fi
+while [ true ]; do
+	ret=$(sudo /boot/firmware/PPPwn/$CPPBIN --interface "$INTERFACE" --fw "${FIRMWAREVERSION//./}" --stage1 "/boot/firmware/PPPwn/stage1_$FIRMWAREVERSION.bin" --stage2 "/boot/firmware/PPPwn/stage2_$FIRMWAREVERSION.bin")
+	if [ $ret -ge 1 ]; then
+		echo -e "\033[32m\nConsole PPPwned! \033[0m\n" | sudo tee /dev/tty1
+		if [ $PPPOECONN = true ]; then
+			if [ $USBETHERNET = true ]; then
+				echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind
+				coproc read -t 3 && wait "$!" || true
+				echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind
+			else
+				sudo ip link set $INTERFACE down
+				coproc read -t 3 && wait "$!" || true
+				sudo ip link set $INTERFACE up
+			fi
 			coproc read -t 3 && wait "$!" || true
 			sudo sysctl net.ipv4.ip_forward=1
 			sudo sysctl net.ipv4.conf.all.route_localnet=1
@@ -134,26 +119,26 @@ if [ $ret -ge 1 ]
 			echo -e "\n\n\033[93m\nPPPoE Enabled \033[0m\n" | sudo tee /dev/tty1
 			sudo pppoe-server -I $INTERFACE -T 60 -N 1 -C PS4 -S PS4 -L 192.168.2.1 -R 192.168.2.2 -F
 		else
-        	if [ $SHUTDOWN = true ] ; then
-        	 coproc read -t 5 && wait "$!" || true
-        	 sudo poweroff
+			if [ $SHUTDOWN = true ]; then
+				coproc read -t 5 && wait "$!" || true
+				sudo poweroff
 			else
-			 sudo ip link set $INTERFACE down
-        	fi
+				sudo ip link set $INTERFACE down
+			fi
 		fi
-        exit 1
-   else
-        echo -e "\033[31m\nFailed retrying...\033[0m\n" | sudo tee /dev/tty1
-		if [ $USBETHERNET = true ] ; then
-		  if [[ ! $UDEV == *"dev/sd"* ]] ;then
-        	echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind
-        	coproc read -t 4 && wait "$!" || true
-        	echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind
-		  fi	
-           else	
-        	sudo ip link set $INTERFACE down
-        	coproc read -t 4 && wait "$!" || true
-        	sudo ip link set $INTERFACE up
-        fi
-fi
+		exit 1
+	else
+		echo -e "\033[31m\nFailed retrying...\033[0m\n" | sudo tee /dev/tty1
+		if [ $USBETHERNET = true ]; then
+			if [[ ! $UDEV == *"dev/sd"* ]]; then
+				echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind
+				coproc read -t 4 && wait "$!" || true
+				echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind
+			fi
+		else
+			sudo ip link set $INTERFACE down
+			coproc read -t 4 && wait "$!" || true
+			sudo ip link set $INTERFACE up
+		fi
+	fi
 done
