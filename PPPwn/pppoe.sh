ae82b473a9469f10c604eb54c5a2937daf4f0fcc
stooged <stooged@users.noreply.github.com>
2024-06-01 07:35:52 +0800

    pppoe
    purge interface on pppoe load

--- PPPwn/pppoe.sh
+++ PPPwn/pppoe.sh
@@ -4,7 +4,18 @@ if [ -f /boot/firmware/PPPwn/config.sh ]; then
 source /boot/firmware/PPPwn/config.sh
 fi
 if [ -z $INTERFACE ]; then INTERFACE="eth0"; fi
-
+if [ -z $USBETHERNET ]; then USBETHERNET=false; fi
+if [ $USBETHERNET = true ] ; then
+	echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind >/dev/null
+	coproc read -t 1 && wait "$!" || true
+	echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind >/dev/null
+	coproc read -t 1 && wait "$!" || true
+	sudo ip link set $INTERFACE up
+   else	
+	sudo ip link set $INTERFACE down
+	coproc read -t 2 && wait "$!" || true
+	sudo ip link set $INTERFACE up
+fi
 sudo iptables -P INPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -P OUTPUT ACCEPT
