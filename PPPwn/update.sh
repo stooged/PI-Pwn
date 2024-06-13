b3c75ec69410051f413803a20abffac663cc82fb
stooged <stooged@users.noreply.github.com>
2024-06-13 10:36:31 +0800

    Update update.sh
    update script to update goldhen if usb drive is being used.

--- PPPwn/update.sh
+++ PPPwn/update.sh
@@ -17,6 +17,29 @@ echo "Starting update..." | sudo tee /dev/tty1 | sudo tee /dev/pts/* | sudo tee 
 sudo systemctl stop pipwn
 echo "Installing files... " | sudo tee /dev/tty1 | sudo tee /dev/pts/* | sudo tee -a /boot/firmware/PPPwn/upd.log
 sudo cp -r PPPwn /boot/firmware/
+FOUND=0
+readarray -t rdirarr  < <(sudo ls /media/pwndrives)
+for rdir in "${rdirarr[@]}"; do
+  readarray -t pdirarr  < <(sudo ls /media/pwndrives/${rdir})
+  for pdir in "${pdirarr[@]}"; do
+     if [[ ${pdir,,}  == "payloads" ]] ; then 
+	   FOUND=1
+	   PSDRIVE='/media/pwndrives/'${rdir}
+	   break
+    fi
+  done
+    if [ "$FOUND" -ne 0 ]; then
+      break
+    fi
+done  
+if [[ ! -z $PSDRIVE ]] ;then
+  if [ -f "USB Drive/goldhen.bin" ]; then
+     if [ -f $PSDRIVE'/goldhen.bin' ]; then
+	   sudo rm -f $PSDRIVE'/goldhen.bin'
+     fi
+     sudo cp "USB Drive/goldhen.bin" $PSDRIVE
+  fi
+fi
 cd /boot/firmware/PPPwn
 sudo chmod 777 *
 sudo bash install.sh update
