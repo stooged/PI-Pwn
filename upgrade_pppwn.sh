05038e1e62043b81df3f6d723e5a4e6fe6e0061c
Nicklosss <129866880+Nicklosss@users.noreply.github.com>
2024-09-19 17:59:39 +0200

    Update upgrade_pppwn.sh

--- upgrade_pppwn.sh
+++ upgrade_pppwn.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 #A simple way to upgrade pppwn version without c/p the entire bash code. Simply give run permissions and execute it into your embedded system.
+sudo apt update
+sudo apt install git -y
+sudo rm -f -r PI-Pwn
+sudo systemctl stop pipwn
+git clone https://github.com/stooged/PI-Pwn
+sudo mkdir /boot/firmware/
+cd PI-Pwn
+sudo cp -r PPPwn /boot/firmware/
+cd /boot/firmware/PPPwn
+sudo chmod 777 *
+sudo bash install.sh
